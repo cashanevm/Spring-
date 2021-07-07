ea8da2088a300da2c1e3729c1be68652fa7414c9
cashanevm <cashanevm@gmail.com>
2021-07-07 09:46:16 +0300

    commit

--- src/main/java/com/example/letscode/Controller/UserController.java
+++ src/main/java/com/example/letscode/Controller/UserController.java
@@ -4,14 +4,19 @@ import com.example.letscode.Domain.Role;
 import com.example.letscode.Domain.User;
 import com.example.letscode.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/user")
+@PreAuthorize("hasAuthority('ADMIN')")
 public class UserController {
     @Autowired
     private UserRepository userRepository;
@@ -25,5 +30,28 @@ public class UserController {
         model.addAttribute("user",user);
         model.addAttribute("roles", Role.values());
         return "userEdit";
+    }
+    @PostMapping
+    public String userSave(
+            @RequestParam("userId") User user,
+            @RequestParam Map<String,String> form,
+            @RequestParam String username
+                           ){
+        user.setUsername(username);
+        Set<String> roles = Arrays.stream(Role.values())
+                .map(Role::name)
+                .collect(Collectors.toSet());
+
+
+
+        user.getRoles().clear();
+        for (String key : form.keySet()) {
+            if (roles.contains(key)){
+                user.getRoles().add(Role.valueOf(key));
+            }
+        }
+        userRepository.save(user);
+
+        return "redirect:/user";
     }
 }
