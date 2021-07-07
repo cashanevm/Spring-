af7a19b687df8ed1fe828700984f3b9051450424
cashanevm <cashanevm@gmail.com>
2021-07-07 15:06:04 +0300

    commit

--- src/main/java/com/example/letscode/Controller/UserController.java
+++ src/main/java/com/example/letscode/Controller/UserController.java
@@ -2,68 +2,66 @@ package com.example.letscode.Controller;
 
 import com.example.letscode.Domain.Role;
 import com.example.letscode.Domain.User;
-import com.example.letscode.Repository.UserRepository;
 import com.example.letscode.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Arrays;
 import java.util.Map;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/user")
-@PreAuthorize("hasAuthority('ADMIN')")
+
 public class UserController {
-    @Autowired
-    private UserRepository userRepository;
 
     @Autowired
     private UserService userService;
-
+    @PreAuthorize("hasAuthority('ADMIN')")
     @GetMapping
     public String userList(Model model){
-    model.addAttribute("users",userRepository.findAll());
+    model.addAttribute("users",userService.findAll());
         return "user-list";
     }
+    @PreAuthorize("hasAuthority('ADMIN')")
     @GetMapping("{user}")
     public String userEditForm(@PathVariable User user, Model model){
         model.addAttribute("user",user);
         model.addAttribute("roles", Role.values());
         return "userEdit";
     }
-
+    @PreAuthorize("hasAuthority('ADMIN')")
     @GetMapping("/delete/{user}")
     public String userDelete(@PathVariable User user){
         userService.deleteUser(user);
         return "redirect:/user";
     }
-
+    @PreAuthorize("hasAuthority('ADMIN')")
     @PostMapping
     public String userSave(
             @RequestParam("userId") User user,
             @RequestParam Map<String,String> form,
             @RequestParam String username
                            ){
-        user.setUsername(username);
-        Set<String> roles = Arrays.stream(Role.values())
-                .map(Role::name)
-                .collect(Collectors.toSet());
-
-
-
-        user.getRoles().clear();
-        for (String key : form.keySet()) {
-            if (roles.contains(key)){
-                user.getRoles().add(Role.valueOf(key));
-            }
-        }
-        userRepository.save(user);
-
+        userService.saveUser(user,username, form);
         return "redirect:/user";
+    }
+
+    @GetMapping("profile")
+    public String getProfile(Model model, @AuthenticationPrincipal User user){
+        model.addAttribute("username",user.getUsername());
+        model.addAttribute("email", user.getEmail());
+        model.addAttribute("password", user.getPassword());
+        return "profile";
+    }
+    @PostMapping("profile")
+    public String updateProfile(@AuthenticationPrincipal User user,
+                                @RequestParam(name="password") String password,
+                                @RequestParam(name="email") String email){
+
+        userService.updateProfile(user, password, email);
+        return "redirect:/user/profile";
     }
 }
